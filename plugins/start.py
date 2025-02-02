a425423ee97debcb8f9c4142e5858f9a18cfaa1d
CoderRzps <ayushpritysingh098@gmail.com>
2025-02-02 18:49:01 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -166,8 +166,8 @@ async def start_command(client: Client, message: Message):
             [
 
     [
-                    InlineKeyboardButton("⚡️ ᴀʙᴏᴜᴛ", callback_data = "about"),
-                    InlineKeyboardButton('🍁 sᴇʀɪᴇsғʟɪx', url='https://t.me/Team_Netflix/40')
+                  #  InlineKeyboardButton("⚡️ ᴀʙᴏᴜᴛ", callback_data = "about"),
+                    InlineKeyboardButton('🍁 sᴇʀɪᴇsғʟɪx', url='https://t.me/AdultXGalleryy')
 
     ]
             ]
